d6f19981469ab32c02e4393f094dbd6aae38d6c2
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-08 22:13:48 +0900

    ForLoopExample

--- ForLoopExample.java
+++ ForLoopExample.java
@@ -2,18 +2,8 @@ import java.util.Scanner;
 
 public class ForLoopExample {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("숫자를 입력하세요: ");
-        int n = sc.nextInt();
-        int sum = 0, i = 0;
-
-        while (i <= n) {
-            sum += i;
-            i++;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
         }
-
-        System.out.println("1부터" + n + "까지의 합: " + sum);
-        sc.close();
     }
 }
