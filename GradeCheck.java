516e89c4df4a113416777e1640323af80dd08c8d
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-08 20:54:33 +0900

    GradeCheck

--- GradeCheck.java
+++ GradeCheck.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class practice {
+public class GradeCheck {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -10,11 +10,11 @@ public class practice {
         if (score >= 90) {
             System.out.println("학점: A");
         } else if (score >= 80) {
-            System.out.println("B");
+            System.out.println("학점: B");
         }else if (score >= 70) {
-            System.out.println("C");
+            System.out.println("학점: C");
         } else if (score >= 60) {
-            System.out.println("D");
+            System.out.println("학점: D");
         } else {
             System.out.println("학점 : F (불합격)");
         }
