91ec70dc2ee4204e8037f743c4662dac6071cf17
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-09 15:36:19 +0900

    Main_4

--- Main_4.java
+++ Main_4.java
@@ -1,9 +1,14 @@
 public class Main_4 {
     public static void main(String[] args) {
-        int a = 7; // 정수 선언 및 초기화
-        int result1 = a << 2;
-        int b = -5;
+        int a = 7; // 정수 선언 및 초기화. 00000111
+        int result1 = a << 2; // 00011100 = 4+8+16 = 28
+        int b = -5; //???모름
         int result2 = b >> 2;
-        int c =
+        int c = 15; //????
+        int result3 = c >>> 2;
+
+        System.out.println("7 << 2 =" + result1);
+        System.out.println("-5 >> 2 =" + result2);
+        System.out.println("15 >>> 2 =" + result3);
     }
 }
