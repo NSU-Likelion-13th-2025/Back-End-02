b12b58f73d3a285fc02bfd293f6aa4bd8ffeb2da
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-09 15:53:31 +0900

    Main_7

---
+++ Main_7.java
@@ -0,0 +1,16 @@
+public class Main_7 {
+    public static void main(String[] args) {
+
+        int a = 10; //변수 선언 및 초기화
+        a += 5; System.out.println("a += 5 : " + a); // a = a+5 // a += 5 : 15 출력
+
+        a -= 3; System.out.println("a -= 3 : " + a); // a = a-3 // a -= 3 : 12 출력
+
+        a *= 2; System.out.println("a *= 2 : " + a); // a = a*2 // a *= 2 : 24 출력
+
+        a /= 4; System.out.println("a /= 4 : " + a); // a = a/4 // a /= 4 : 6 출력
+
+        a %= 5; System.out.println("a %= 5 : " + a); // a = a%5 // a %=5 : 1 출력
+
+    }
+}
