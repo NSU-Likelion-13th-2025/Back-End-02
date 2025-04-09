6649cb675c8f4964db4d9c1ef8fac43773eb110a
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-09 15:40:11 +0900

    Main_5

--- Main_4.java
+++ Main_4.java
@@ -1,6 +1,6 @@
 public class Main_4 {
     public static void main(String[] args) {
-        int a = 7; // 정수 선언 및 초기화. 00000111
+        int a = 7; // 변수 선언 및 초기화. 00000111
         int result1 = a << 2; // 00011100 = 4+8+16 = 28
         int b = -5; //???모름
         int result2 = b >> 2;
---
+++ Main_5.java
@@ -0,0 +1,12 @@
+public class Main_5 {
+    public static void main(String[] args) {
+        int a = 6; // 변수 선언 및 초기화
+        int b = 4;
+        System.out.println(a<b); // a는 b보다 작다
+        System.out.println(a>b); // a는 b보다 크다
+        System.out.println(a>=b); // a는 b이상
+        System.out.println(a<=b); // a는 b이하
+        System.out.println(a==b); // a는 b와 같다
+        System.out.println(a!=b); // a는 b와 같지 않다
+    }
+}
