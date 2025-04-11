677a85160b14de868c32ab29d3df400ef0e883b9
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-11 16:49:05 +0900

    Main_9

--- Main_9.java
+++ Main_9.java
@@ -1,10 +1,10 @@
 public class Main_9 {
     public static void main(String[] args) {
 
-        int a = (true)? 1:2;
+        int a = (true)? 1:2; // 조건식 (true)가 참. 1이 변수 a에 할당.
         System.out.println(a);
 
-        int b = (a % 2 == 0)? 10 : 20;
+        int b = (a % 2 == 0)? 10 : 20; // 조건식이 false이므로 20이 변수 b에 할당.
         System.out.println(b);
     }
 }
