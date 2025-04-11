a9aade805d58c9acd3e4fe203f05d3858a58790a
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-11 18:05:38 +0900

    SumCalculatorWhile

--- ForLoopExample.java
+++ ForLoopExample.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class ForLoopExample {
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++) {
-            System.out.println(i);
+        for (int i = 1; i <= 10; i++) { // 처음 i =1, int = int + 1와 int가 10이하일때 까지 실행 구문을 반복.
+            System.out.println(i); // i값을 출력함
         }
     }
 }
--- SumCalculatorWhile.java
+++ SumCalculatorWhile.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 public class SumCalculatorWhile {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // Scanner 객체 생성. 화면으로부터 값을 입력받음
 
-        System.out.print("숫자를 입력하세요: ");
-        int n = sc.nextInt();
-        int sum = 0, i = 1;
+        // 초기식
+        System.out.print("숫자를 입력하세요: "); // ln이 없으므로 행바꿈 x
+        int n = sc.nextInt(); // 입력받은 숫자를 n에 저장
+        int sum = 0, i = 1; // 변수 선언 및 초기화
 
-        while (i <= n) {
+        while (i <= n) { // 조건식. i이 n이하일 때
             sum += i; // sum = sum + i
-            i++;
+            i++; // i는 1씩 증가
         }
 
         System.out.println("1부터" + n + "까지의 합: " + sum);
-        sc.close();
+        sc.close(); // Scanner 사용 이후 sc.close()로 닫기.
     }
 }
