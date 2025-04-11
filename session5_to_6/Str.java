7a3b473eec03b502c2fc1dbc280b217e2cea5702
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-12 03:21:02 +0900

    Str

---
+++ session5_to_6/Str.java
@@ -0,0 +1,10 @@
+package session5_to_6;
+
+public class Str {
+    public static void main(String[] args) {
+        String a = "apple"; // 변수 선언 및 초기화. 문자열
+        String b = "apple";
+
+        System.out.println(a == b); // 문자열 리터럴을 바로 입력해 객체를 생성할 때 같은 문자열끼리 객체를 공유
+    }
+}
