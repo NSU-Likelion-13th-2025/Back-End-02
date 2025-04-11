77792f37402dfbfa7948777925e3c01a455c9e48
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-11 16:45:55 +0900

    Main_8

--- Main_8.java
+++ Main_8.java
@@ -1,9 +1,12 @@
 public class Main_8 {
     public static void main(String[] args) {
 
-        int b = 8;
-        b &= 3; System.out.println("b &= 3 : " + b);
-        b |= 2; System.out.println("b |= 2 : " + b);
-        b ^= 6; System.out.println("b ^= 6 : " + b);
+        int b = 8; // 00001000
+        b &= 3; System.out.println("b &= 3 : " + b); // 3 : 00000011 이고 b &= 3 : 00000000
+        b |= 2; System.out.println("b |= 2 : " + b); // 2 : 00000010 이고 b |= 2 : 00000010
+        b ^= 6; System.out.println("b ^= 6 : " + b); // 6 : 00000110 이고 b ^= 6 : 00000100
+        b <<= 1; System.out.println("b <<= 1 : " + b); // 00001000 왼쪽으로 1칸 밈.
+        b >>= 2; System.out.println("b >>= 2 : " + b); // 00000010 오른쪽으로 2칸 밈.
+        b >>>= 2; System.out.println("b >>>= 2 : " + b); // >>> : 논리 쉬프트. 다 0
     }
 }
---
+++ Main_9.java
@@ -0,0 +1,10 @@
+public class Main_9 {
+    public static void main(String[] args) {
+
+        int a = (true)? 1:2;
+        System.out.println(a);
+
+        int b = (a % 2 == 0)? 10 : 20;
+        System.out.println(b);
+    }
+}
