8bccbd8204ded579c1530cbae483443fc3c204cc
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-11 16:57:55 +0900

    AgeCheck

--- AgeCheck.java
+++ AgeCheck.java
@@ -2,16 +2,16 @@ import java.util.Scanner;
 
 public class AgeCheck {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in); // Scanner클래스의 객체를 생성? 화면에서 입력받음.
 
         System.out.print("나이를 입력하세요: ");
-        int age = sc.nextInt(); // 사용자 입력
+        int age = sc.nextInt(); // 입력받은 내용 -> age 저장
 
-        if (age >= 20) {
+        if (age >= 20) { // (조건식)인 age가 20이상이면 "당신은 성인입니다"가 출력하고, 아니면 실행구문이 실행되지 않음.
             System.out.println("당신은 성인입니다.");
         }
 
         System.out.println("프로그램 종료.");
-        sc.close();
+        sc.close(); // Scanner 사용 이후 sc.close()로 닫기.
     }
 }
