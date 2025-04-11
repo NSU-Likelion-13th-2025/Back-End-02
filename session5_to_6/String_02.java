5aea94059e4f1458908526d96a1b3bd6e58e5bee
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-12 03:37:44 +0900

    String_02

---
+++ session5_to_6/String_02.java
@@ -0,0 +1,10 @@
+package session5_to_6;
+
+public class String_02 {
+    public static void main(String[] args) {
+        System.out.println(1 + "안녕"); // 숫자열 + 문자열
+        System.out.println(1 + "안녕" + 2); // 숫자열 + 문자열 + 문자열
+        System.out.println("안녕" + 1 + 2); // 문자열 + 문자열 + 문자열
+        System.out.println(1 + 2 + "안녕"); // 숫자열 + 숫자열 + 문자열
+    }
+}
