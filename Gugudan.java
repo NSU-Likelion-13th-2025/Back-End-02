1e258deae960c7a2e804434f48d71bc6c9745a40
yunwoo2510 <25102583@nsu.ac.kr>
2025-04-11 18:17:18 +0900

    Gugudan

--- Gugudan.java
+++ Gugudan.java
@@ -1,8 +1,8 @@
 public class Gugudan {
     public static void main(String[] args) {
-        for (int dan = 2; dan <= 9; dan++) {
+        for (int dan = 2; dan <= 9; dan++) { // 변수 선언및 초기화. 초기식 dan에 2할당. dan이 9이하일때 반복. dan은 실행이후 1씩 증가.
             System.out.println("=== " + dan + "단 ===");
-            for (int i = 1; i <= 9; i++) {
+            for (int i = 1; i <= 9; i++) { // 위와 동일. i에 1할당. i는 9이하일때 반복. i는 실행이후 1씩 증가.
                 System.out.println(dan + " x " + i + " = " + (dan * i));
             }
             System.out.println(); // 단마다 줄바꿈
