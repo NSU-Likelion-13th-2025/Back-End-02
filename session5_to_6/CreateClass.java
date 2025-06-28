2eb14f9bb1451fa5c013b320a16db2598b4fe340
yunwoo2510 <25102583@nsu.ac.kr>
2025-06-29 02:52:57 +0900

    yunwoo

--- session5_to_6/CreateClass.java
+++ session5_to_6/CreateClass.java
@@ -2,7 +2,7 @@ package session5_to_6;
 
 class A{ // 클래스 A는 바로 사용할 수 없음
 
-    int m = 5; // 변수 선언 및 초기화
+    int m = 5; // 필드
 
     void print(){
         // 매서드 활용
@@ -11,15 +11,11 @@ class A{ // 클래스 A는 바로 사용할 수 없음
 }
 
 public class CreateClass {
-    public static void main(String[] args) {
-        A a = new A();
-        // new 연산자
-        // 객체 생성
-        a.m = 6;
-        // 객체. 객체변수 = 값
-        System.out.println(a.m);
+    public static void main(String[] args) {        //main 메서드
+        A a = new A();          // 생성자. 객체a 생성
+        a.m = 6;                // 필드값 초기화
+        System.out.println(a.m);  //6출력
 
-        a.print();
-        // 매서드 활용
+        a.print();              //객체a의 메서드print() 활용
     }
 }
