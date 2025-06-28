05a76334b02a1bb45ed358755594e255183958ae
yunwoo2510 <25102583@nsu.ac.kr>
2025-06-29 05:19:17 +0900

    yunwoo

---
+++ session7/Method1_1.java
@@ -0,0 +1,30 @@
+package session7;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class Method1_1 {
+    //방법1
+    public static void main(String[] args) {
+        //계산1
+        int a = 1;
+        int b = 2;
+        System.out.println(a + "+" + b + "연산 수행");
+        int sum1 = a + b;
+        System.out.println("결과1 출력:" + sum1);
+
+        //계산2
+        int x = 10;
+        int y = 20;
+        System.out.println(x + "+" + y + " 연산 수행");
+        int sum2 = x + y;
+        System.out.println("결과2 출력:" + sum2);
+    }
+
+    //방법2
+    public static int add(int a, int b) {
+        System.out.println(a + "+" + b + " 연산 수행");
+        int sum = a + b;
+        System.out.println(a + "+" + b + " 결과 = " + sum);
+        return sum;
+    }
+}
