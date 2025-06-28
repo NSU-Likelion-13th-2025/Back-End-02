b5969d00eb9ca5b8f1b4eb1206932a53f10046c9
yunwoo2510 <25102583@nsu.ac.kr>
2025-06-29 04:45:58 +0900

    yunwoo

--- session7/Method1.java
+++ session7/Method1.java
@@ -1,27 +1,18 @@
 package session7;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Method1 {
-    public static void main(String[] args) {
-        // 미리 정의된 메서드
+    public static void main(String[] args) { //미리 정의된 메서드. main() 메서드
         int a = 1;
         int b = 2;
-        System.out.println(a + "+" + b + " 연산 수행"); // 1+2 연산 수행
-        int sum1 = a + b;
-        System.out.println("결과1 출력:" + sum1); // 결과1 출력:3
-
-        // 계산2
-        int x = 10;
-        int y = 20;
-        System.out.println(x + "+" + y + " 연산 수행"); // 10+20 연산 수행
-        int sum2 = x + y;
-        System.out.println("결과2 출력:" + sum2); // 결과2 출력:30
+        System.out.println(a + "+" + b + "연산 수행"); // main()메서드만 출력
     }
 
     public static int add(int a, int b) {
-        // 사용자 정의 메서드
-        System.out.println(a + "+" + b + " 연산 수행");
+        //사용자 정의 메서드. add()메서드는 a,b를 매개변수로 받아 int형의 sum값을 리턴
+        System.out.println(a+ "+" + b + "연산 수행");
         int sum = a + b;
-        System.out.println(a + "+" + b + " 결과 = " + sum);
-        return sum; // 메서드 실행결과 반환
+        return sum;                         // return 리턴값;
     }
 }
