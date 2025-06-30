4051c851412256aa5877e3c77266bba43d0b9a7a
yunwoo2510 <25102583@nsu.ac.kr>
2025-06-30 17:20:23 +0900

    yunwoo

--- session7/this1.java
+++ session7/this1.java
@@ -1,16 +1,20 @@
 package session7;
 
 class A {
-    int a;
-    int b;
+    // 필드
+    int a; //멤버변수 a
+    int b; //멤버변수 b
 
-    void init(int a, int b) {
-        a = a;
-        b = b;
+    // 메소드를 통한 필드값 선언
+    void init(int a, int b) {       //매개변수 a, b
+        //this : 멤버 변수명과 매개 변수명이 동일
+        this.a = a;
+        this.b = b;
     }
 
+    // 메소드를 통한 필드값 선언
     void work() {
-        this.init(a, b);
+        this.init(2, 3);        // this : 자신의 객체에 접근
     }
 }
 
--- session7/this2.java
+++ session7/this2.java
@@ -1,22 +1,23 @@
-//package session7;
-//
-//class B {
-//    B() {
-//        System.out.println("B() 생성자");
-//    }
-//
-//    B(int a) {
-//        this();
-//        System.out.println("B(int a) 생성자");
-//    }
-//}
-//
-//public class this2 {
-//    public static void main(String[] args) {
-//        B b1 = new B();
-//        System.out.println();
-//        B b2 = new B(3);
-//        System.out.println();
-//        B b3 = new B(5, 1);
-//    }
-//}
+package session7;
+
+class B{
+    B() {
+        System.out.println("B() 생성자");
+    }
+
+    B(int a) {
+        //this()
+        //생성자 코드에서만 사용가능. 가장 윗줄 위치
+        this();                      //같은 클래스에서 생성자가 다른 생성자를 호출
+        System.out.println("B(" + a + ") 생성자");
+    }
+}
+
+public class this2 {
+    public static void main(String[] args) {
+        //객체 생성
+        B b1 = new B();             //B() 생성자 호출
+        System.out.println();
+        B b2 = new B(3);         //B(int a)생성자 호출
+    }
+}
